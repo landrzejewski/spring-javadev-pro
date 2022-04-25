c3a4fecc801364b1481956d8e57ace8195c83767
landrzejewski <landrzejewski.poczta@gmail.com>
2022-04-25 14:34:25 +0200

    update

--- src/main/java/pl/training/shop/payments/ConsolePaymentLogger.java
+++ src/main/java/pl/training/shop/payments/ConsolePaymentLogger.java
@@ -29,7 +29,7 @@ public class ConsolePaymentLogger {
     }
 
     private String createLogEntry(Payment payment) {
-        return String.format(LOG_FORMAT, payment.getValue());
+        return LOG_FORMAT.formatted(payment.getValue());
     }
 
     @PostConstruct
