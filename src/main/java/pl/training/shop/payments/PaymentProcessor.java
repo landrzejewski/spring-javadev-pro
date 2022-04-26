9a7d87b555391ab33dc1eaf451068c1a8daab209
landrzejewski <landrzejewski.poczta@gmail.com>
2022-04-26 09:59:38 +0200

    update

--- src/main/java/pl/training/shop/payments/PaymentProcessor.java
+++ src/main/java/pl/training/shop/payments/PaymentProcessor.java
@@ -5,6 +5,8 @@ import org.javamoney.moneta.FastMoney;
 import org.springframework.stereotype.Component;
 import pl.training.shop.time.TimeProvider;
 
+import java.time.Instant;
+
 @Component
 @RequiredArgsConstructor
 public class PaymentProcessor implements PaymentService {
@@ -18,14 +20,18 @@ public class PaymentProcessor implements PaymentService {
 
     @Override
     public Payment process(PaymentRequest paymentRequest) {
-        var totalPaymentValue = calculateTotalPaymentValue(paymentRequest.getValue());
-        var payment = Payment.builder()
-                .id(paymentIdGenerator.getNext())
-                .value(totalPaymentValue)
-                .timestamp(timeProvider.getTimestamp())
-                .status(DEFAULT_PAYMENT_STATUS)
-                .build();
+        var paymentValue = calculateTotalPaymentValue(paymentRequest.getValue());
+        var payment= createPayment(paymentValue);
         return paymentsRepository.save(payment);
+    }
+
+    private Payment createPayment(FastMoney paymentValue) {
+        return Payment.builder()
+                .id(paymentIdGenerator.getNext())
+                .value(paymentValue)
+                .timestamp(Instant.now())
+                .status(PaymentStatus.STARTED)
+                .build();
     }
 
     private FastMoney calculateTotalPaymentValue(FastMoney paymentValue) {
